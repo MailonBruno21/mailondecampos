0ac61fd5adfe3226863be3e7b46b30412861f997
MailonBruno21 <pedrir.campos@outlook.com>
2022-01-15 18:52:57 -0400

    Nova versão do site utilizando CSS Grid

--- script/script.js
+++ script/script.js
@@ -1,18 +1,58 @@
 //Efeito do texto digitando na tela 
-function escrevendoTexto(e, velocidade){
-    const textoArray = e.innerHTML.split('');
-    e.innerHTML = '';
 
-    for(let i = 0; i < textoArray.length; i++) {
-        setTimeout(() => e.innerHTML += textoArray[i], velocidade * i);
+function escrevendoTexto(e, velocidade){
+    for(let z = 0; z < e.length; z++){
+        const textoArray = e[z].innerHTML.split('');
+        textoArray[z].innerHTML;
+
+        e[z].innerHTML = '';
+
+        for(let i = 0; i < textoArray.length; i++){
+            
+            setTimeout(function(){
+                
+                e[z].innerHTML += textoArray[i]
+                
+            }, velocidade * i)
+        }
     }
     
 }
 
-const textoApresentacao = document.querySelector('#animacao-apresentacao')
-escrevendoTexto(textoApresentacao, 40)
-const textoBemVindo = document.querySelector('#animacao-boasvindas')
-escrevendoTexto(textoBemVindo, 64)
+const textoApresentacao = document.querySelectorAll(".animacao-texto-digitando")
+escrevendoTexto(textoApresentacao, 50)
+
+//--------------------------------------------
+//animacao icons tecnologia
+
+function animandoIcons(e){
+    for(let i = 0; i < e.length; i++){
+
+        e[i].style.marginBottom = "-150px"
+        console.log(e[i])
+        let aux = 0;
+        for(let z = -80; z < 60; z++){
+            aux++
+            setTimeout(function(){
+                e[i].style.marginBottom = z +"px"
+            },10*aux)
+        }
+    }
+}
+
+function iconsTecnologiaDesce(e){
+    e.style.marginBottom = "60px"
+}
+
+function iconsTecnologiaSobe(e){
+    e.style.marginBottom = "64px"
+}
+
+
+
+const animacaoIcons = document.querySelectorAll(".img-icons-tecnologias img")
+
+animandoIcons(animacaoIcons)
 
 //Menu dropdown com clique
 function menuDrop() {
